58a5723183168bee9ecf523f17fd741d2f01a06b
Shahed Ahmed <shahedahmedutsho@gmail.com>
2024-07-08 19:36:36 +0600

    complite about

--- src/Pages/Home/AboutSection/AboutSection.jsx
+++ src/Pages/Home/AboutSection/AboutSection.jsx
@@ -4,12 +4,14 @@ const AboutSection = () => {
     return (
         <div className=' min-h-screen bg-gray-500  lg:py-20 md:py-10 py-5  '>
             <div className='lg:container lg:mx-auto flex flex-col lg:flex-row md:mx-8 mx-4 gap-8 '>
-            <div  className=' lg:min-h-screen bg-slate-50 gap-2   flex lg:block  w-full'>
+            <div  className=' lg:min-h-screen  gap-2   flex lg:block  w-full'>
             <div className="object-cover w-4/12  lg:w-full gap-2    lg:h-[600px]">
             <img className=' object-cover w-full h-full ' src="https://res.cloudinary.com/dmcmxmqpw/image/upload/f_auto,q_auto/dr4lctx0s4me5ncsv2gr" alt="" />
 </div>
-<div className="object-cover lg:mt-8 w-8/12  lg:h-[400px] lg:w-[107%]  left-[-3.2vw] lg:relative ">
+<div className='w-8/12 md:w-full  relative'>
+<div className="object-cover lg:mt-8 w-full  lg:h-[400px] lg:w-[107%]  right-0 lg:absolute  ">
             <img className=' object-cover h-full w-full  ' src="https://res.cloudinary.com/dmcmxmqpw/image/upload/f_auto,q_auto/wbx0qy6l1lsb23nii3tv" alt="" />
+</div>
 </div>
 
             </div>
